6c42918228ddac9a9f8870ff09ab13a8b3ad3368
Sushnata Sarkar <coolsushnata@gmail.com>
2024-11-15 09:42:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+  type Query {
+    status: Boolean
+  }
+`;
+
+const resolver = {
+    Query:{
+        status:()=>{
+            return true;
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs : typeDefs,
+    resolvers: resolver,
+    plugins:[ApolloServerPluginLandingPageGraphQLPlayground()]
+});
+
+ //server.listen(4000);
+ server.listen().then(({url})=>{
+ console.log(`server ready at ${url}`)
+ });
